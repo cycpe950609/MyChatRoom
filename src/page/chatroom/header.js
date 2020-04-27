58f60ed9c2373035b20e8fdef07abf6c7ebe6e9b
Cycpe950609 <cycpe950609@gmail.com>
2020-04-28 00:51:42 +0800

    Add Header Icon curse

--- src/page/chatroom/header.js
+++ src/page/chatroom/header.js
@@ -9,6 +9,7 @@ export class Header extends React.Component
     { 
         super(props);
         this.SignOutProcess = this.SignOutProcess.bind(this);
+        this.btnIcon_onClick = this.btnIcon_onClick.bind(this);
     }
 
     render()
@@ -16,7 +17,7 @@ export class Header extends React.Component
         return(
             <div className="header">
                 <nav className="navbar mt-auto navbar-light bg-light">
-                    <div className="chatroom_header_icon"  alt=""/>
+                    <div className="chatroom_header_icon"  alt="" onClick={this.btnIcon_onClick}/>
                     <span className="mx-auto my-0" id="chatroom_header_title">User's Name</span>
                     <ul className="nav navbar-nav navbar-right">
                         <li><button className="btn btn-dark" ><img src="./img/logout.png" width="30" height="30" alt=""/></button></li>
@@ -24,6 +25,11 @@ export class Header extends React.Component
                 </nav>    
             </div>
         );
+    }
+
+    btnIcon_onClick(e)
+    {
+        alert('Btn Click');
     }
 
     SignOutProcess()
